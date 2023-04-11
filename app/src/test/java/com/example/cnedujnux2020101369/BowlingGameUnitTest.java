3c24a92e5f5c437ab0510d93b6a903297ee00eb2
cn.edu.jnu.x2020101369 <1493478554@qq.com>
2023-04-11 16:37:57 +0800

    test2_n

--- app/src/test/java/com/example/cnedujnux2020101369/BowlingGameUnitTest.java
+++ app/src/test/java/com/example/cnedujnux2020101369/BowlingGameUnitTest.java
@@ -1,5 +1,6 @@
 package com.example.cnedujnux2020101369;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -10,14 +11,34 @@ import static org.junit.Assert.*;
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 public class BowlingGameUnitTest {
+
+    private BowlingGame game;
+
+    @Before
+    public void setUp() throws Exception {
+        game = new BowlingGame();
+    }
+
     @Test
     public void first_test()
     {
-        BowlingGame game = new BowlingGame();
-        for (int i = 0;i < 20;i++)
-        {
-            game.roll(0);
-        }
+        repeatedRoll(0, 20);
         assertEquals(0,game.score());
     }
+
+    @Test
+    public void second_test()
+    {
+        repeatedRoll(1,20);
+        assertEquals(20,game.score());
+    }
+
+    private void repeatedRoll(int pin, int times) {
+        for (int i = 0;i < times;i++)
+        {
+            game.roll(pin);
+        }
+    }
+
+
 }
