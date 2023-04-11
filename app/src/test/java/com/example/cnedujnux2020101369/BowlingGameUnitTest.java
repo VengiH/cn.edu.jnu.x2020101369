eb0c51377363364e72a6bb8158620a557da2035b
cn.edu.jnu.x2020101369 <1493478554@qq.com>
2023-04-11 16:21:11 +0800

    test1

---
+++ app/src/test/java/com/example/cnedujnux2020101369/BowlingGameUnitTest.java
@@ -0,0 +1,23 @@
+package com.example.cnedujnux2020101369;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class BowlingGameUnitTest {
+    @Test
+    public void first_test()
+    {
+        BowlingGame game = new BowlingGame();
+        for (int i = 0;i < 20;i++)
+        {
+            game.roll(0);
+        }
+        assertEquals(0,game.score());
+    }
+}
